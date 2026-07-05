b2625c32fac96011feba71b99d13b19e0ebabc9a
xaxaqqq <alicerinnya@gmail.com>
2026-07-05 20:21:34 +0800

    修改網頁錯誤

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/signPdf/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
